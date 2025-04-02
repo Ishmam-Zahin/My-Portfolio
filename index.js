38d5a407178b9bf732058b8aace6291a0a92b1e8
ishmam-zahin <ishmamzahin404@gmail.com>
2025-04-02 22:47:18 +0600

    initial

---
+++ index.js
@@ -0,0 +1,72 @@
+const home = document.getElementsByClassName("home")[0];
+const home1 = home.getElementsByClassName("home-container--1")[0];
+const home2 = home.getElementsByClassName("home-container--2")[0];
+const navbar = document.getElementsByClassName("nav-bar")[0];
+const navButton = document.getElementsByClassName("btn-nav")[0];
+let homeVisible = false;
+let navBarOpen = false;
+let homeTimer = null;
+
+const homeOvserver = new IntersectionObserver(entries => {
+    if(entries[0].isIntersecting){
+        homeVisible = true;
+        start();
+    }
+    else{
+        homeVisible = false;
+        stopToggleHome();
+    }
+}, {
+    threshold: 0,
+    rootMargin: "0px 0px 0px 0px",
+});
+
+homeOvserver.observe(home);
+
+function start(){
+    homeTimer = setTimeout(() => {
+        invisible();
+    }, 8000);
+}
+
+function invisible(){
+    home1.classList.toggle("invisible");
+    home2.classList.toggle("invisible");
+
+    setTimeout(() => {
+        toggleHome();
+    }, 500);
+}
+
+function toggleHome(){
+    home1.classList.toggle("hide");
+    home2.classList.toggle("hide");
+
+    if(homeVisible){
+        start();
+    }
+}
+
+function stopToggleHome(){
+    if(homeTimer) clearTimeout(homeTimer);
+}
+
+
+
+function toggleNav(){
+    navButton.classList.toggle("btn-nav--cross");
+    navbar.classList.toggle("nav-hide");
+    if(navBarOpen){
+        navBarOpen = false;
+    }
+    else{
+        navBarOpen = true;
+    }
+}
+
+
+window.addEventListener("hashchange", () => {
+    if(navBarOpen){
+        toggleNav();
+    }
+})
