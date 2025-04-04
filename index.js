617cc97068cf94dfd2cc1689b999269ffbdffea3
ishmam-zahin <ishmamzahin404@gmail.com>
2025-04-04 22:15:29 +0600

    till projects

--- index.js
+++ index.js
@@ -4,10 +4,15 @@ const home2 = home.getElementsByClassName("home-container--2")[0];
 const navbar = document.getElementsByClassName("nav-bar")[0];
 const navButton = document.getElementsByClassName("btn-nav")[0];
 const works = document.getElementsByClassName("works-container")[0].children;
+const projectTypes = document.getElementsByClassName("project-options")[0].children;
+let workImages = null;
+
 let homeVisible = false;
 let navBarOpen = false;
 let homeTimer = null;
 let currentWorks = 0;
+let currentWorkImage = 0;
+let currentActiveLink = "#home";
 
 const homeOvserver = new IntersectionObserver(entries => {
     if(entries[0].isIntersecting){
@@ -68,6 +73,10 @@ function toggleNav(){
 
 
 window.addEventListener("hashchange", () => {
+    document.querySelector(`[href="${currentActiveLink}"]`).classList.toggle("nav-active");
+    currentActiveLink = window.location.hash;
+    document.querySelector(`[href="${currentActiveLink}"]`).classList.toggle("nav-active");
+
     if(navBarOpen){
         toggleNav();
     }
@@ -75,19 +84,49 @@ window.addEventListener("hashchange", () => {
 
 
 function toggleDropDown(element){
+    const headerHeight = element.getElementsByClassName("dropdown__header")[0].clientHeight + 2;
+
     if(!element.classList.contains("dropdown--large")){
         element.classList.toggle("dropdown--large");
-        const height = element.getElementsByClassName("dropdown__text")[0].clientHeight + 2 + 65 + 5;
+        const height = element.getElementsByClassName("dropdown__text")[0].clientHeight + 2 + headerHeight + 5;
         element.style.height = height + "px";
     }
     else{
         element.classList.toggle("dropdown--large");
-        element.style.height = "65px";
+        element.style.height = headerHeight + "px";
     }
 }
 
 function toggleWorks(index){
+    projectTypes[currentWorks].classList.toggle("work-active");
     works[currentWorks].classList.toggle("works-hide");
     currentWorks = index;
+    projectTypes[currentWorks].classList.toggle("work-active");
     works[currentWorks].classList.toggle("works-hide");
+}
+
+function toggleWorkGallery(element){
+    const gallery = element.parentElement.parentElement.nextElementSibling;
+    workImages = gallery.getElementsByTagName("img");
+    currentWorkImage = 0;
+    for(let i = 0; i < workImages.length; i++){
+        if(i == currentWorkImage){
+            workImages[i].classList.remove("invisible");
+        }
+        else{
+            workImages[i].classList.add("invisible");
+        }
+    }
+    gallery.classList.toggle("hide");
+}
+
+function closeWorkGallery(element){
+    const gallery = element.parentElement;
+    gallery.classList.toggle("hide");
+}
+
+function nextImage(){
+    workImages[currentWorkImage].classList.toggle("invisible");
+    currentWorkImage = (currentWorkImage + 1) % workImages.length;
+    workImages[currentWorkImage].classList.toggle("invisible");
 }
